d9a3497550f57129af496a6dd96859f6abd41ad1
jorenvh1 <joren.vh@hotmail.com>
2016-12-19 13:13:07 +0100

    update

---
+++ arduino_files/twitter_test/twitter_test.ino
@@ -0,0 +1,208 @@
+/*
+      Author: Nicholas Bester
+      Title: Twitter Mention Test
+      Version: 0.1
+*/
+
+// Debuging variables
+int const DEBUG = 0; // Test LED without Serial feedback
+
+// LED control
+int ledPin = A1;
+
+// Value sent from Python
+int signalState;
+
+//value to check if loop has finished
+bool allowLoop = true;
+
+// Rood => Rechts
+// Zwart => links
+
+//forward pins => yellow
+int forwardPin1 = 13;
+int forwardPin2 = 9;
+
+//backward pins => yellow
+int backwardPin1 = 12;
+int backwardPin2 = 10;
+
+//left pins
+int leftPin1 = 6;
+int leftPin2 = 4;
+
+//right pins
+int rightPin1 = 7;
+int rightPin2 = 3;
+
+//up pins
+int upPin1 = A0;
+int upPin2 = A5;
+
+//down pins
+int downPin1 = A1;
+int downPin2 = A4;
+
+void setup() {
+  // Transistor pin connection on board
+  pinMode(ledPin, OUTPUT);
+
+  pinMode(forwardPin1, OUTPUT);
+  pinMode(forwardPin2, OUTPUT);
+  pinMode(backwardPin1, OUTPUT);
+  pinMode(backwardPin2, OUTPUT);
+
+  pinMode(leftPin1, OUTPUT);
+  pinMode(leftPin2, OUTPUT);
+  pinMode(rightPin1, OUTPUT);
+  pinMode(rightPin2, OUTPUT);
+
+  pinMode(downPin1, OUTPUT);
+  pinMode(downPin2, OUTPUT);
+  pinMode(upPin1, OUTPUT);
+  pinMode(upPin2, OUTPUT);
+
+
+  // Enabling communication
+  Serial.begin(9600);
+  // Test breadboard setup to LED
+  if (DEBUG) {
+    tweetReceived();
+  }
+
+
+}
+
+void loop() {
+  if (!DEBUG) {
+    if (Serial.available()) {
+      byte receivedValue = Serial.read() - '0';
+      signalState = receivedValue;
+
+      if (signalState == 1) {
+        tweetReceived();
+      }
+
+      else if (signalState == 0) {
+        ledToggle(false);
+      }
+
+      Serial.flush();
+    }
+  }
+}
+
+// Flash the light when tweet is received
+void tweetReceived() {
+  /*for (int i = 0; i < 10; i++) {
+    ledToggle(true);
+    delay(100);
+    ledToggle(false);
+    delay(100);
+  }*/
+   if(allowLoop) {
+    allowLoop = false;
+    goForward(3500);
+    goRight(5000);
+    goDown(4500);
+    goUp(4500);
+    goLeft(4800); //geen 5000 => gaat anders net iets te ver
+    goBackwards(3300); //geen 3500 => gaat anders net iets te ver
+    goDown(1000);
+    goUp(1000);
+    allowLoop = true;
+  }
+}
+
+// turn LED on and off
+void ledToggle(boolean value) {
+  if (value) {
+    analogWrite(ledPin, 1023);
+  } else {
+    analogWrite(ledPin, 0);
+  }
+}
+
+void goForward(int amountOfMS) {
+  delay(500); //making sure not to short out the circuit
+  digitalWrite(backwardPin1, LOW);
+  digitalWrite(backwardPin2, LOW);
+  digitalWrite(forwardPin1, HIGH);
+  digitalWrite(forwardPin2, HIGH);
+  delay(amountOfMS);
+  //making sure all outputs are LOW again after action
+  digitalWrite(backwardPin1, LOW);
+  digitalWrite(backwardPin2, LOW);
+  digitalWrite(forwardPin1, LOW);
+  digitalWrite(forwardPin2, LOW);
+}
+
+void goBackwards(int amountOfMS) {
+  delay(500); //making sure not to short out the circuit
+  digitalWrite(backwardPin1, HIGH);
+  digitalWrite(backwardPin2, HIGH);
+  digitalWrite(forwardPin1, LOW);
+  digitalWrite(forwardPin2, LOW);
+  delay(amountOfMS);
+  //making sure all outputs are LOW again after action
+  digitalWrite(backwardPin1, LOW);
+  digitalWrite(backwardPin2, LOW);
+  digitalWrite(forwardPin1, LOW);
+  digitalWrite(forwardPin2, LOW);
+}
+
+void goLeft(int amountOfMS) {
+  delay(500); //making sure not to short out the circuit
+  digitalWrite(leftPin1, HIGH);
+  digitalWrite(leftPin2, HIGH);
+  digitalWrite(rightPin1, LOW);
+  digitalWrite(rightPin2, LOW);
+  delay(amountOfMS);
+  //making sure all outputs are LOW again after action
+  digitalWrite(leftPin1, LOW);
+  digitalWrite(leftPin2, LOW);
+  digitalWrite(rightPin1, LOW);
+  digitalWrite(rightPin2, LOW);
+}
+
+void goRight(int amountOfMS) {
+  delay(500); //making sure not to short out the circuit
+  digitalWrite(rightPin1, HIGH);
+  digitalWrite(rightPin2, HIGH);
+  digitalWrite(leftPin1, LOW);
+  digitalWrite(leftPin2, LOW);
+  delay(amountOfMS);
+  //making sure all outputs are LOW again after action
+  digitalWrite(leftPin1, LOW);
+  digitalWrite(leftPin2, LOW);
+  digitalWrite(rightPin1, LOW);
+  digitalWrite(rightPin2, LOW);
+}
+
+void goDown(int amountOfMS) {
+  delay(500); //making sure not to short out the circuit
+  digitalWrite(downPin1, HIGH);
+  digitalWrite(downPin2, HIGH);
+  digitalWrite(upPin1, LOW);
+  digitalWrite(upPin2, LOW);
+  delay(amountOfMS);
+  //making sure all outputs are LOW again after action
+  digitalWrite(downPin1, LOW);
+  digitalWrite(downPin2, LOW);
+  digitalWrite(upPin1, LOW);
+  digitalWrite(upPin2, LOW);
+}
+
+void goUp(int amountOfMS) {
+  delay(500); //making sure not to short out the circuit
+  digitalWrite(upPin1, HIGH);
+  digitalWrite(upPin2, HIGH);
+  digitalWrite(downPin1, LOW);
+  digitalWrite(downPin2, LOW);
+  delay(amountOfMS);
+  //making sure all outputs are LOW again after action
+  digitalWrite(downPin1, LOW);
+  digitalWrite(downPin2, LOW);
+  digitalWrite(upPin1, LOW);
+  digitalWrite(upPin2, LOW);
+}
---
+++ twitterapi/twitterapi.py
@@ -0,0 +1,97 @@
+# Author: Nicholas Bester
+# Title: Twitter Stream connection
+# Description: Tracks a string using the Twitter API and sends a Serial command to an Arduino once a Tweet matching the tracked string is found
+# Version: 1.1
+
+# imports
+import time
+from time import sleep
+from TwitterAPI import TwitterAPI
+import struct
+import os
+from serial import Serial
+import httplib
+from httplib import IncompleteRead
+
+# Pretty console colours
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# Variables
+availableArduino = True # Debugging without an Arduino
+testSerial = False # Debugging without Twitter connection
+arduinoPort = 'COM3' # USB port address for the Arduino
+arduinoBaud = '9600' # Baud for serial communication
+arduinoWaitTime = 3 # The length of time Python wait before attemping to issue commands to the Arduino
+stringToTrack = "#TomEnCoGift" # Change this to the search term you wish to track from Twitter
+
+# Access requirements for Twitter API connection
+access_token = '339797949-hmyahYIUlGO6377imBE420DFAQwJGmOrkzUgCW9k'
+access_token_secret = 'XgC7Y2f303QzWZB5mtRZ6orqZ8qmoY8eErabpUXSOu96g'
+consumer_key = '45k9ChfSdUVvvXxoh0DCACehw'
+consumer_secret = 'QrHerJxIT3U6BXIbWCoybyPi5FNFDoDfaxGxrGOeV6O8pdm9i5'
+
+# Clearing the screen for aesthetic display of console statements
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print bcolors.WARNING + "Initialising Twitter Stream Application" + bcolors.ENDC
+
+print bcolors.OKGREEN + "Initialisation OK!" + bcolors.ENDC
+
+print bcolors.WARNING + 'Initialising Arduino Board through Serial' + bcolors.ENDC
+
+# Arduino serial communication
+if availableArduino:
+    ser = Serial(arduinoPort, arduinoBaud, timeout=3)
+
+# Gives the Arduino board time to initialise
+sleep(arduinoWaitTime)
+
+# Testing serial send to Arduino (ensure available Arduino is True)
+if testSerial:
+    print "On"
+    ser.write(bytes(1))
+    sleep(arduinoWaitTime)
+    print "Off"
+    ser.write(bytes(0))
+    sleep(arduinoWaitTime)
+else:
+    print bcolors.OKGREEN + "Initialisation OK!" + bcolors.ENDC
+    print bcolors.WARNING + 'Initialising Twitter Stream API Authorisation' + bcolors.ENDC
+    try:
+        # Setting up a connection to Twitter using the Twitter API
+        api = TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
+
+        print bcolors.OKGREEN + "Initialisation OK!" + bcolors.ENDC
+        print bcolors.OKBLUE + 'Twitter Stream Request running' + bcolors.ENDC
+
+        # A request object which opens a stream to Twitter tracking the hashtag in question
+        r = api.request('statuses/filter', {'track':stringToTrack})
+
+        # Checks if text within the stream item is populated and issues a command to the Arduino
+        for item in r.get_iterator():
+            if 'text' in item:
+                print item['user']['screen_name'].encode('utf-8') + ' tweeted: ' + item['text'].encode('utf-8')# Print screen name and the tweet text
+
+                # It is possible to check the tweets for further commands using regular expressions to send multiple commands to the Arduino
+
+                if availableArduino:
+                    print "Activating claw machine"
+                    ser.write(bytes(1)) # The command is a simple byte intepretation of the integer 1
+                    sleep(arduinoWaitTime) # Wait before sending next command
+                    ser.write(bytes(0))
+                    sleep(arduinoWaitTime) # Wait before sending next command
+    except IncompleteRead:
+        # Oh well, reconnect and keep trucking
+        print "IncompleteRead occurred"
+    except KeyboardInterrupt:
+        # Or however you want to exit this loop
+        api.disconnect()
+        exit()
