c8c6c256d2f6c4524676ab2c562e157a922c5354
jorenvh1 <joren.vh@hotmail.com>
2017-01-09 15:54:01 +0100

    changesss bebebebeeeeee

--- claw-site/resources/views/onepage.blade.php
+++ claw-site/resources/views/onepage.blade.php
@@ -39,7 +39,17 @@
                         <strong>#TomEnCoGift</strong>. Wanneer men dit deed werd er een 'claw-machine' geactiveerd die voor uw trouwe viervoeter enkele snoepjes cadeau gaf.
                         Mensen konden ook vrijblijvend een bijdrage leveren voor De Warmste Week.
                     </p>
-                    <h3 class="text-center mg-lg">Laatste 5 bijdragers:</h3>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- header END -->
+    <div class="bloc l-bloc bgc-grey" id="services">
+        <div class="container bloc-lg">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1 class="text-center mg-lg">Laatste 5 bijdragers:</h1>
                     <ul class="rolldown-list" id="myList">
                         @foreach($lastUsers as $user)
                             <li class="tc-white">&#64;{{$user->twitterName}}</li>
@@ -49,22 +59,78 @@
             </div>
         </div>
     </div>
-    <!-- header END -->
-    <div class="bloc l-bloc bgc-platinum" id="services">
-        <div class="container bloc-lg">
+    <!-- services -->
+    <div class="bloc bgc-carmine-pink d-bloc" id="team">
+        <div class="container bloc-sm">
             <div class="row">
                 <div class="col-sm-12">
-                    <h3 class="text-center mg-lg">De Feed-by-Tweet machine in actie!</h3>
+                    <h1 class="text-center mg-md ">
+                        The project
+                    </h1>
+                </div>
+            </div>
+            <div class="row voffset-lg">
+                <div class="col-sm-6">
+                    <img src="img/feedbytweetmachine.png" class="creator img-responsive animDelay04 animated zoomIn" alt="feed by tweet machine"/>
+                </div>
+                <div class="col-sm-6 top-buffer">
+                    <p class="bigger-text">De fysieke wereld linken met de virtuele wereld. Dat was onze missie. Waarom dit niet combineren met geld inzamelen voor een goed doel?</p>
+                    <p class="bigger-text">We stonden te popelen om deze kleine grijpmachine te gebruiken wat eigenlijk een mini snoepjesgrijper was.</p>
+                </div>
+            </div>
 
+            <div class="row voffset-lg top-buffer">
+                <div class="col-sm-6">
+                    <img src="img/claw_machine_open.jpg" class="creator img-responsive animDelay04 animated zoomIn smaller" alt="feed by tweet machine open"/>
+                </div>
+                <div class="col-sm-6 top-buffer">
+                    <p class="bigger-text">First things first. Het eerste obstakel was de machine openhalen en de bedrading tackelen. Na vele testjes 'hackten' we deze machine. Hierna werd een Arduino gebruikt om hier instructies naar te sturen.</p>
+                    <p class="bigger-text">Iedereen die voorbij onze stand wandelde en graag een hondensnoepje voor hun trouwe viervoeter wou, kon een tweet sturen met de hashtag <strong>#TomEnCoGift</strong>. Een klein Python scriptje scande Twitter voor deze hashtag en stuurde de Arduino een signaal wanneer een nieuwe tweet ontvangen werd.</p>
+                </div>
+            </div>
+
+            <div class="row voffset-lg top-buffer">
+                <div class="col-sm-6 top-buffer">
+                    
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- team -->
+    <div class="bloc bgc-grey d-bloc" id="team">
+        <div class="container bloc-sm">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1 class="text-center tc-darkgrey mg-md ">
+                        Team
+                    </h1>
+                </div>
+            </div>
+            <div class="row voffset-lg">
+                <div class="col-sm-6">
+                    <img src="img/luka.jpg" class="creator img-responsive animDelay04 animated zoomIn" alt="member"/>
+
+                    <h3 class="text-center tc-darkgrey mg-lg">
+                        Luka verhoeven
+                    </h3>
+                </div>
+                <div class="col-sm-6">
+                    <img src="img/joren.jpg" class="creator img-responsive animDelay06 animated zoomIn" alt="member"/>
+
+                    <h3 class="text-center tc-darkgrey mg-lg">
+                        Joren Van Hemelrijk
+                    </h3>
                 </div>
             </div>
         </div>
     </div>
-    <!-- services -->
-    <div class="bloc l-bloc bgc-white" id="services">
+    <!-- team END -->
+
+    <div class="bloc l-bloc bgc-blue" id="services">
         <div class="container bloc-lg">
             <div class="row">
-                <h3 class="text-center mg-lg">Partners</h3>
+                <h1 class="text-center mg-lg">In samenwerking met</h1>
                 <div class="col-sm-6">
                     <div class="text-center">
                         <span class="et-icon-linegraph icon-xl icon-carmine-pink animated zoomIn animDelay02"><img class="mfl" src="img/musicforlife.png" alt=""></span>
@@ -92,36 +158,6 @@
             </div>
         </div>
     </div>
-
-    <!-- team -->
-    <div class="bloc bgc-carmine-pink d-bloc" id="team">
-        <div class="container bloc-sm">
-            <div class="row">
-                <div class="col-sm-12">
-                    <h2 class="text-center mg-md ">
-                        Team
-                    </h2>
-                </div>
-            </div>
-            <div class="row voffset-lg">
-                <div class="col-sm-6">
-                    <img src="img/luka.jpg" class="creator img-responsive animDelay04 animated zoomIn" alt="member"/>
-
-                    <h3 class="text-center mg-lg">
-                        Luka verhoeven
-                    </h3>
-                </div>
-                <div class="col-sm-6">
-                    <img src="img/joren.jpg" class="creator img-responsive animDelay06 animated zoomIn" alt="member"/>
-
-                    <h3 class="text-center mg-lg">
-                        Joren Van Hemelrijk
-                    </h3>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- team END -->
 
     <!-- ScrollToTop Button -->
     <a class="bloc-button btn btn-d scrollToTop" onclick="scrollToTarget('1')"><span
